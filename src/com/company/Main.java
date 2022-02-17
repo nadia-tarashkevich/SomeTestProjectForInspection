bc9ad54a9827b84768aad6e158551afcc1f66919
Nadezhda Solovyeva <nadia.solovyeva@jetbrains.com>
2022-02-18 00:13:29 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String s1 = "My string";
+        String s2 = "My string";
+        boolean stringsAreTheSame = s1 == s2;
+    }
+}
